e1be98faa6cafa95cc931f0c6c69854e086a7ad9
NotDeoxide <bryanjonathan87293@gmail.com>
2026-03-04 17:12:32 -0700

    added comment

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -3,7 +3,7 @@ package com.example.lab08;
 public class City {
     private String city;
     private String province;
-
+// add comment
     public City(String city, String province){
         this.city = city;
         this.province = province;
